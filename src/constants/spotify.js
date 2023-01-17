822321dcbc2fed605f109b0f532904d71ec8c141
Vanessa Muratore <m.vanessa.dev@gmail.com>
2023-01-17 13:11:16 +0000

    Redirect setup

--- src/constants/spotify.js
+++ src/constants/spotify.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = '9f38e34f65dd42c685e38b5b703f82ef'
-const REDIRECT_URI = 'http://192.168.0.9:3000'
+const REDIRECT_URI = 'https://vans-m.github.io/name-three-songs'
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
 const RESPONSE_TYPE = 'token'
 
